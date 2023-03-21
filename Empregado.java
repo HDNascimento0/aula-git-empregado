e9131c34850896a4de254f9de0405ecb700a069c
Henrique Nascimento <henrique4562@hotmail.com>
2023-03-20 21:14:05 -0300

    Primeiro teste concluído com sucesso

--- Empregado.java
+++ Empregado.java
@@ -31,23 +31,62 @@ public class Empregado{
     public String getNome() {
         return nome;
     }
+
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
+
     public int getIdade() {
         return idade;
     }
+
+
     public void setIdade(int idade) {
         this.idade = idade;
     }
+
 
     public int getTipo() {
         return tipo;
     }
 
+
     public void setTipo(int tipo) {
         this.tipo = tipo;
     }
+
+
+    public double getSalario() {
+        return salario;
+    }
+
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+
+    public double getComissao() {
+        return comissao;
+    }
+
+
+    public void setComissao(double comissao) {
+        this.comissao = comissao;
+    }
+
+
+    public double getBonus() {
+        return bonus;
+    }
+
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
 
 
 
